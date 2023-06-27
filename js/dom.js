c4e40e2e667b4b8ec7f27806ba7c666a85015696
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-06-26 20:26:37 -0500

    Doom

---
+++ js/dom.js
@@ -0,0 +1,5 @@
+/*let texto = "Hola, soy Sergio Bonilla y quiero aprender a programar en JavaScript";
+const habla = (texto) => {
+    speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+}
+habla(texto); */
