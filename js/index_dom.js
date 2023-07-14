ad41749f682cf587df5836bb777343ae0731a910
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-07-14 09:08:42 -0500

    reloj digital

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,7 +1,9 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
+import { digitalClock, alarm } from "./dom/reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
 });
