0150fdc689a50616b913be7726ad004ab02efe35
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-07-19 14:57:00 -0500

    juego

--- js/dom/teclado.js
+++ js/dom/teclado.js
@@ -1,6 +1,43 @@
 const d = document;
+let x = 0, 
+    y = 0;
 
-export function shortcuts (e){
+export function moveBall(e, ball, stage){
+    const $ball = d.querySelector(ball), 
+        $stage = d.querySelector(stage);
+    
+    limitsBall = $ball.getBoundingClientRect();
+    limitsStage = $stage.getBoundingClientRect();
+
+    //const move = (direction) =>{}
+    switch (e.keyCode) {
+        case 37: //left mouse
+        //  move("left");
+        e.preventDefault();
+        if(limitsBall.left > limitsStage.left) x--;
+        break;
+        case 38: //up mouse
+        e.preventDefault();
+        if(limitsBall.up > limitsStage.up) y--;
+        //move("up");
+        break;
+        case 39: //right mouse
+        //move("right");
+        e.preventDefault();
+        if(limitsBall.right > limitsStage.right) x++;
+        break;
+        case 40: //down mouse   
+        //move("down");
+        e.preventDefault();
+        if(limitsBall.down > limitsStage.down) y++;
+        break;
+        default:
+            break;
+    }
+    $ball.style.transform = `translate(${x*10}px, ${y*10}px)`; 
+}
+
+export function shortcuts(e){
     /*console.log(e);
     console(e.type);
     console.log(e.key);
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,6 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
-import {shortcuts} from "./dom/teclado.js";
+import {moveBall, shortcuts} from "./dom/teclado.js";
 
 const d = document;
 
@@ -12,4 +12,5 @@ d.addEventListener("DOMContentLoaded", (e) => {
 
 d.addEventListener("keydown", (e)=>{
     shortcuts(e);
+    moveBall(e, ".ball", ".stage");
 });
