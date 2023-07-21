f57639921d9c4a26cc83a83eb24ec438d9c02313
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-07-21 09:36:21 -0500

    scroll

--- js/index_dom.js
+++ js/index_dom.js
@@ -2,6 +2,7 @@ import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import {moveBall, shortcuts} from "./dom/teclado.js";
 import countdown from "./dom/cuenta_regresiva.js";
+import scrollTopButton from "./dom/boton_scroll.js";
 
 const d = document;
 
@@ -10,6 +11,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/Naruto.mp3", "#activar-alarma", "#desactivar-alarma");
     countdown("countdown", "December 31, 2023 23:59:59", "Feliz Navidad Pendejo");
+    scrollTopButton (".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e)=>{
