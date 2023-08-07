bd2cf34b7b64b0fb37e5f1c7c32460e84a66dadb
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-08-07 11:10:18 -0500

    deteccion_camara

---
+++ js/dom/deteccion_webcam.js
@@ -0,0 +1,20 @@
+const d = document,
+    n = navigator;
+
+export default function webCam(id){
+    const $video = d.getElementById(id);
+
+    if(n.mediaDevices.getUserMedia){
+        n.mediaDevices
+            .getUserMedia({video: true, audio: false})
+            .then((stream) => {
+                console.log(stream);
+                $video.srcObject = stream;
+                $video.play();
+            })
+            .catch((err) => {
+                $video.insertAdjacentHTML('beforebegin', `<p><mark>¡Sucedio el siguiente error!: ${err}</mark></p>`);
+                console.log(`¡Sucedio el siguiente error!: ${err}`);
+            })
+    }
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -8,6 +8,7 @@ import responsiveMedia from "./dom/objeto_responsive.js";
 import responsiveTester from "./dom/prueba_responsive.js";
 import userDeviceInfo from "./dom/deteccion_dispositivos.js";
 import networkStatus from "./dom/deteccion_red.js";
+import webCam from "./dom/deteccion_webcam.js";
 
 const d = document;
 
@@ -31,6 +32,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     );
     responsiveTester("responsive-tester");
     userDeviceInfo("user-device");
+    webCam("webcam");
 });
 
 d.addEventListener("keydown", (e)=>{
