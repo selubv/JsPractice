19275b21e73442d7c3502f1597f2f61c27a4637e
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-08-15 11:30:19 -0500

    video_inteligente

--- js/index_dom.js
+++ js/index_dom.js
@@ -14,6 +14,7 @@ import searchFilter from "./dom/filtros_busquedas.js";
 import draw from "./dom/sorteo.js";
 import slider from "./dom/carrucel.js";
 import scrollSpy from "./dom/scroll_espia.js";
+import smartVideo from "./dom/video_inteligente.js";
 
 const d = document;
 
@@ -43,6 +44,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     draw("#winner-btn", ".player");
     slider();
     scrollSpy();
+    smartVideo();
 });
 
 d.addEventListener("keydown", (e)=>{
