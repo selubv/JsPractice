cd92d8734fdf32ded7c18fde8c4c2ee993ee1a38
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-08-25 17:12:05 -0500

    json

--- js/ajax.js
+++ js/ajax.js
@@ -51,3 +51,39 @@
         })
         .finally(()=> console.log("Esto se ejecutará independientemente del resultado del API FETCH"));
 })();
+
+(()=>{
+    const $fetchAsync = document.getElementById("fetch-async"),
+        $fragment = document.createDocumentFragment();
+
+    async function getData(){
+        try{
+            let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+                json = await res.json();
+
+            if(!res.ok) throw {status: res.status, statusText: res.statusText};
+
+            json.forEach((el)=>{
+                const $li = document.createElement("li");
+
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            $fetchAsync.appendChild($fragment);
+        } catch(err){
+            let message = err.statusText || "Ocurrio un error";
+
+            $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
+        } finally{
+            console.log("Esto se ejecutará independientemente del try... catch");
+        }
+    }
+
+    getData();
+})();
+
+(()=>{
+    const $axios = document.getElementById("axios"),
+        $fragment = document.createDocumentFragment();
+})();
