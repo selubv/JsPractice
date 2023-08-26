36125d3770d06ff27c2d8ce55b520d901373250a
Sergio Bonilla <selubv0111bonilla@gmail.com>
2023-08-25 21:14:02 -0500

    axios

--- js/ajax.js
+++ js/ajax.js
@@ -86,4 +86,27 @@
 (()=>{
     const $axios = document.getElementById("axios"),
         $fragment = document.createDocumentFragment();
+
+    axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res)=>{
+            let json = res.data;
+
+            json.forEach((el) => {
+                const $li = document.createElement("li");
+
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            $axios.appendChild($fragment);
+        })
+        .catch((err)=>{
+            let message = err.response.statusText || "Ocurrio un error";
+
+            $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+        })
+        .finally(() => {
+            console.log("Esto se ejecuta independientemente del resultado del Axios");
+        });
 })();
